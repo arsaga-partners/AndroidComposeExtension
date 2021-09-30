9c5e7a11aa1127621307837cef157a79e2a05500
内田正輝 <masaki.u@arsaga.jp>
2021-09-30 20:52:36 +0900

    FragmentとComposeを繋ぐ関数を作成

---
+++ src/main/java/jp/co/arsaga/extensions/compose/screen/Fragment.kt
@@ -0,0 +1,19 @@
+package jp.co.arsaga.extensions.compose.screen
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+fun Fragment.composable(
+    fragmentId: Int,
+    content: @Composable () -> Unit
+): View = ComposeView(requireContext()).apply {
+    id = fragmentId
+    layoutParams = ViewGroup.LayoutParams(
+        ViewGroup.LayoutParams.MATCH_PARENT,
+        ViewGroup.LayoutParams.MATCH_PARENT
+    )
+    setContent(content)
+}
