3dd8ded9ed63e43fd9f6ac29161614529aecab8c
内田正輝 <masaki.u@arsaga.jp>
2021-10-21 21:20:15 +0900

    無限スクロールの位置に応じて処理を発火させるComposable関数を作成

---
+++ src/main/java/jp/co/arsaga/extensions/compose/screen/InfinityScroll.kt
@@ -0,0 +1,35 @@
+package jp.co.arsaga.extensions.compose.screen
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.*
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+
+@InternalCoroutinesApi
+@Composable
+fun InfiniteListHandler(
+    listState: LazyListState,
+    buffer: Int,
+    onLoadMore: () -> Unit
+) {
+    val loadMore = remember {
+        derivedStateOf {
+            val layoutInfo = listState.layoutInfo
+            val totalItemsNumber = layoutInfo.totalItemsCount
+            val lastVisibleItemIndex = (layoutInfo.visibleItemsInfo.lastOrNull()?.index ?: 0) + 1
+
+            lastVisibleItemIndex > (totalItemsNumber - buffer)
+        }
+    }
+
+    LaunchedEffect(loadMore) {
+        snapshotFlow { loadMore.value }
+            .distinctUntilChanged()
+            .filter { it }
+            .collect {
+                onLoadMore()
+            }
+    }
+}
