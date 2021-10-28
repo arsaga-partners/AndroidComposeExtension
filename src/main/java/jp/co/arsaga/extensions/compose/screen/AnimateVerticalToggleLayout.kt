2c188d0436b012c305e25d5e18e1430ebd215af1
内田正輝 <masaki.u@arsaga.jp>
2021-10-28 14:37:04 +0900

    上下に開閉できる画面パーツのレイアウトを作成

---
+++ src/main/java/jp/co/arsaga/extensions/compose/screen/AnimateVerticalToggleLayout.kt
@@ -0,0 +1,34 @@
+package jp.co.arsaga.extensions.compose.screen
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.IntSize
+
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun AnimateVerticalToggleLayout(
+    isVisible: Boolean,
+    direction: Alignment.Vertical,
+    dampingRatio: Float = Spring.DampingRatioNoBouncy,
+    content: @Composable () -> Unit
+) {
+    val animationSpec = spring(
+        dampingRatio = dampingRatio,
+        visibilityThreshold = IntSize.VisibilityThreshold
+    )
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = expandVertically(expandFrom = direction, animationSpec = animationSpec),
+        exit = shrinkVertically(shrinkTowards = direction, animationSpec = animationSpec)
+    ) {
+        content()
+    }
+}
