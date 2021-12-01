2f024fab61a32784bf6017e997dd3362610a4bb5
内田正輝 <masaki.u@arsaga.jp>
2021-12-01 13:40:47 +0900

    キーボード連動やシステムUIのPaddingなどは全案件で使うので共通化

--- src/main/java/jp/co/arsaga/extensions/compose/screen/Fragment.kt
+++ src/main/java/jp/co/arsaga/extensions/compose/screen/Fragment.kt
@@ -2,9 +2,16 @@ package jp.co.arsaga.extensions.compose.screen
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.navigationBarsWithImePadding
+import com.google.accompanist.insets.statusBarsPadding
 
 fun Fragment.composable(
     fragmentId: Int,
@@ -16,4 +23,21 @@ fun Fragment.composable(
         ViewGroup.LayoutParams.MATCH_PARENT
     )
     setContent(content)
+}
+
+@Composable
+fun SystemPaddingLayout(
+    content: @Composable () -> Unit
+) {
+    ProvideWindowInsets(windowInsetsAnimationsEnabled = true) {
+        Box(
+            contentAlignment = Alignment.TopCenter,
+            modifier = Modifier
+                .fillMaxSize()
+                .statusBarsPadding()
+                .navigationBarsWithImePadding()
+        ) {
+            content()
+        }
+    }
 }
