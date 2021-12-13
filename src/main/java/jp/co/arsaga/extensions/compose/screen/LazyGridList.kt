87b3dbf0133d08c7f00b3cceb6eb4c4f89762d72
内田正輝 <masaki.u@arsaga.jp>
2021-12-13 17:42:59 +0900

    gridItemsのRowの中を更新させるための仕組みを追加

--- src/main/java/jp/co/arsaga/extensions/compose/screen/LazyGridList.kt
+++ src/main/java/jp/co/arsaga/extensions/compose/screen/LazyGridList.kt
@@ -7,39 +7,55 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import timber.log.Timber
 import kotlin.math.ceil
 
 @Deprecated("公式がexperimentalじゃなくなったら削除")
 inline fun <T> LazyListScope.gridItems(
     items: List<T>,
     columnCount: Int,
+    updateNotifier: Any = Any(),
     crossinline rowModifier: (Int) -> Modifier = { Modifier },
     crossinline itemLayout: @Composable RowScope.(T) -> Unit
-) = gridItems(items, columnCount, rowModifier) { item, _, _ ->
+) = gridItems(items, columnCount, updateNotifier, rowModifier) { item, _, _ ,_->
     itemLayout(item)
 }
 @Deprecated("公式がexperimentalじゃなくなったら削除")
 inline fun <T> LazyListScope.gridItems(
     items: List<T>,
     columnCount: Int,
+    updateNotifier: Any = Any(),
     crossinline rowModifier: (Int) -> Modifier = { Modifier },
-    crossinline itemLayout: @Composable RowScope.(T, Int, Int) -> Unit
+    crossinline itemLayout: @Composable RowScope.(T, Any) -> Unit
+) = gridItems(items, columnCount, updateNotifier, rowModifier) { item, _, _ ,notifier->
+    itemLayout(item, notifier)
+}
+@Deprecated("公式がexperimentalじゃなくなったら削除")
+inline fun <T> LazyListScope.gridItems(
+    items: List<T>,
+    columnCount: Int,
+    updateNotifier: Any = Any(),
+    crossinline rowModifier: (Int) -> Modifier = { Modifier },
+    crossinline itemLayout: @Composable RowScope.(T, Int, Int, Any) -> Unit
 ) {
     val rowCount = ceil(items.size / columnCount.toDouble()).toInt()
+    Timber.d("テスト通過１")
     return items(
         count = rowCount,
         key = { index: Int -> index }
     ) { rowPosition ->
+        Timber.d("テスト通過２")
         val groupedFirstPosition = rowPosition * columnCount
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .then(rowModifier(rowPosition))
         ) {
+            Timber.d("テスト通過３")
             repeat(columnCount) { columnPosition ->
                 val position = groupedFirstPosition + columnPosition
                 items.getOrNull(position)?.let {
-                    itemLayout(it, rowPosition, columnPosition)
+                    itemLayout(it, rowPosition, columnPosition, updateNotifier)
                 } ?: run {
                     Spacer(modifier = Modifier.weight(1F))
                 }
