bf1507aa8b5cf953b64fbaf14f867c38d80b2091
内田正輝 <masaki.u@arsaga.jp>
2021-12-13 17:46:49 +0900

    無駄なログを削除

--- src/main/java/jp/co/arsaga/extensions/compose/screen/LazyGridList.kt
+++ src/main/java/jp/co/arsaga/extensions/compose/screen/LazyGridList.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import timber.log.Timber
 import kotlin.math.ceil
 
 @Deprecated("公式がexperimentalじゃなくなったら削除")
@@ -39,19 +38,16 @@ inline fun <T> LazyListScope.gridItems(
     crossinline itemLayout: @Composable RowScope.(T, Int, Int, Any) -> Unit
 ) {
     val rowCount = ceil(items.size / columnCount.toDouble()).toInt()
-    Timber.d("テスト通過１")
     return items(
         count = rowCount,
         key = { index: Int -> index }
     ) { rowPosition ->
-        Timber.d("テスト通過２")
         val groupedFirstPosition = rowPosition * columnCount
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .then(rowModifier(rowPosition))
         ) {
-            Timber.d("テスト通過３")
             repeat(columnCount) { columnPosition ->
                 val position = groupedFirstPosition + columnPosition
                 items.getOrNull(position)?.let {
